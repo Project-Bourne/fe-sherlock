871edca06084fa088015db29a4e80ce998b99b88
chisom chima <chimachisom360@gmail.com>
2023-10-11 07:18:51 +0100

    Added assessment to the ui

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,6 @@ import { useRouter } from "next/router";
 import FileUpload from "./components/FileUpload";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import BreadCrum from "../../components/ui/Breadcrumbs";
-import Min_and_Max_icon from "./components/Min_Max_icon";
 import DummyText from "./components/dummyText";
 import { useDispatch, useSelector } from 'react-redux';
 import { setTextandTitle } from "../../redux/reducer/analyzerSlice";
@@ -12,9 +10,7 @@ import AuthService from "../../services/auth.service";
 import NotificationService from "../../services/notification.service";
 import { setUserInfo } from "../../redux/reducer/authReducer";
 import ActionIcons from "./components/actionIcons/ActionIcons";
-import { Box, CircularProgress } from "@mui/material";
-import CustomModal from "../../components/ui/CustomModal";
-import Loader from '../../components/ui/Loader'
+import { Box } from "@mui/material";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
@@ -113,7 +109,6 @@ function Home() {
             <div>
               <ActionIcons docId={analyzedUuid} />
               <div className="bg-white border mt-3 mx-10 rounded-[1rem]">
-                {/* <Min_and_Max_icon maxOnClick={handleMax} minOnClick={handleMin} /> */}
                 {hideMeta == true && (
                   <div className="pl-5 p-5">
                     <p className="text-md text-gray-500">Title</p>
@@ -122,7 +117,7 @@ function Home() {
                     </h1>
                   </div>
                 )}
-                {hideMeta == false && ( //hide and show meta data
+                {hideMeta == false && (
                   <h1 className="md:text-lg font-bold pl-5 pb-2">
                     {analyzedTitle}
                   </h1>
@@ -150,7 +145,24 @@ function Home() {
         </CustomTabPanel>
       </div>
       <CustomTabPanel value={value} index={1}>
-        {assessment}
+        {assessment.length > 0 ?
+          <div className="my-4 px-4 py-2 bg-white border rounded-lg shadow-md leading-7">
+            {assessment}
+          </div> :
+          <div className="flex items-center justify-center flex-col gap-4 h-[60vh]">
+            <div className="flex items-center justify-centery w-[50%] font-bold flex-col p-3 rounded-[1rem] gap-3 text-xl ">
+              <span> <Image
+                src={require(`../../../public/icons/no_history.svg`)}
+                alt="upload image"
+                width={150}
+                height={150}
+                priority
+              /></span>
+              <h1 className="font-[700] text-2xl">No Analysed Assessment yet</h1>
+              <span className='text-gray-400'>Copy and paste content or Upload a file to be Analyzed</span>
+            </div>
+          </div>
+        }
       </CustomTabPanel>
     </div>
   );
