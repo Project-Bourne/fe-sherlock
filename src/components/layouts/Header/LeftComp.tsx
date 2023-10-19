2bf0852c63cf18ccffd8939164b247a1739ea6cd
chisom chima <chimachisom360@gmail.com>
2023-10-19 12:38:19 +0100

    feat

--- src/components/layouts/Header/LeftComp.tsx
+++ src/components/layouts/Header/LeftComp.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const LeftComp = props => {
   return (
     <div className="flex relative flex-row items-center w-full self-start">
-      <div className="bg-sirp-lightGrey flex flex-row w-full mr-2 py-3 px-5 h-[45px] rounded-[12px]">
+      {/* <div className="bg-sirp-lightGrey flex flex-row w-full mr-2 py-3 px-5 h-[45px] rounded-[12px]">
         <Image
           src={require('../../../../public/icons/search.svg')}
           alt="Search"
@@ -20,9 +20,9 @@ export const LeftComp = props => {
             className="w-full bg-sirp-lightGrey border-none outline-none text-[13px]"
           />
         </div>
-      </div>
+      </div> */}
 
-      <div className="bg-sirp-lightGrey cursor-pointer flex py-2 px-2 rounded-[15px] w-[45px] h-[45px] items-center justify-center content-center">
+      {/* <div className="bg-sirp-lightGrey cursor-pointer flex py-2 px-2 rounded-[15px] w-[45px] h-[45px] items-center justify-center content-center">
         <Image
           src={require('../../../../public/icons/filter.svg')}
           alt="Filter"
@@ -32,7 +32,7 @@ export const LeftComp = props => {
           style={{ alignSelf: 'center' }}
           priority
         />
-      </div>
+      </div> */}
     </div>
   );
 };
